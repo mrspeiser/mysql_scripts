0ec46f379c042a937de23970fdc7ea5b0694405d
matthew <mattrspeiser@gmail.com>
2019-06-24 15:48:17 -0600

    added change user password sql script

---
+++ change_user_password.sql
@@ -0,0 +1,5 @@
+ALTER USER 'DATABASE_USER'@'localhost' IDENTIFIED BY 'new_password';
+
+/* FOR VERSIONS OLDER THAN 5.7.5 OR MARIADB 10.1.20 AND OLDER USE:
+	SET PASSWORD FOR 'database_user'@'localhost' = PASSWORD('new_password');
+*/
